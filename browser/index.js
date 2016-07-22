8540bbc428b5ad5fde8cd1dbc16cc560626577bd
Eric Lanehart <pushred@gmail.com>
2016-07-22 14:27:06 -0400

    Improve project details toggling transition
    Measure height of content with JS rather than max-height hack for proper easing
    
    Hide content area until window load

--- browser/index.js
+++ browser/index.js
@@ -6,20 +6,27 @@ var VideoPlayer = require('../blocks/video_player');
 /////////
 
 var logoEl;
+var mainEl;
+var detailsEls;
 
 document.addEventListener('DOMContentLoaded', () => {
   logoEl = dom.find('.header__logo');
+  mainEl = dom.find('main');
+  detailsEls = dom.findAll('.project__details');
+
   dom.classList(logoEl).add('header__logo--loading');
+  dom.classList(mainEl).add('main--loading');
 
   dom.findAll('.project__expand').forEach((el) => {
     el.on('click', (event) => {
       event.preventDefault();
 
       var detailsEl = dom.find(event.target.getAttribute('href'));
-      if (!detailsEl) return;
 
-      dom.classList(detailsEl).add('project__details--open');
-      dom.classList(el).add('project__expand--expanded');
+      if (detailsEl) {
+        dom.classList(el).add('project__expand--expanded');
+        dom.classList(detailsEl).remove('project__details--collapsed');
+      }
     });
   });
 });
@@ -27,10 +34,31 @@ document.addEventListener('DOMContentLoaded', () => {
 window.addEventListener('load', () => {
   dom.classList(logoEl).remove('header__logo--loading');
 
+  measureDetails();
+
+  detailsEls.forEach((el) => {
+    dom.classList(el).add('project__details--collapsed');
+  });
+
+  dom.classList(mainEl).remove('main--loading');
+
   const player = VideoPlayer();
   player.playNearestVideo();
 
   window.addEventListener('scroll', () => {
     player.playNearestVideo();
   });
+
+  window.addEventListener('resize', () => {
+    measureDetails();
+    player.playNearestVideo();
+  });
+
+  dom.classList(mainEl).add('main--loaded');
 });
+
+function measureDetails () {
+  detailsEls.forEach((el) => {
+    el.style.height = el.clientHeight.toString() + 'px';
+  });
+}
